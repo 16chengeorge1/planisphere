d089dd29368308da38b1236ff72efc69d4b005c3
16chengeorge1 <16chengeorge1@gmail.com>
2016-06-30 08:48:40 -0400

    Each node can now only communicate by sending and receiving messages

--- src/osu/planisphere/NumberAdding.java
+++ src/osu/planisphere/NumberAdding.java
@@ -10,32 +10,45 @@ import osu.planisphere.DemoBasic.PingPongNode;
 import java.util.*;
 //im going to start with two nodes, then figure out the algorithm for multiple nodes
 public class NumberAdding {
-public static ArrayList<Integer> sums_from_addingnodes=new ArrayList<Integer>();
 		public static class AddNumberMessage extends Message {
 			private int numsum;
-			public AddNumberMessage(NodeIdentifier sender,  int numsum)
+			private ArrayList<Integer> nums;
+			private ArrayList<ServerNode> servernodelist;
+			private ArrayList<ClientNode> clientnodelist;
+			private Node senderNode;
+			public AddNumberMessage(Node senderNode,  int numsum,ArrayList<Integer> nums, ArrayList<ServerNode> servernodelist, ArrayList<ClientNode>clientnodelist)
 			{
-				super(sender);
+				super(senderNode.getID());
+				this.senderNode=senderNode;
+				this.numsum=numsum;
+				this.nums=nums;
+				this.servernodelist=servernodelist;
+				this.clientnodelist=clientnodelist;
+			}
+			public AddNumberMessage(Node senderNode,int numsum)
+			{
+				super(senderNode.getID());
+				this.senderNode=senderNode;
 				this.numsum=numsum;
 			}
 			@Override
 			public String toString()
 			{
-				return String.valueOf(numsum)+" from "+sender;
+				return String.valueOf(numsum)+" from "+senderNode.getID();
 			}
 		}
 		
-		public static ArrayList<ArrayList<Integer>> dividing_nums_for_summing(ArrayList<AddingNode> nodelist, ArrayList<Integer> nums)
+		public static ArrayList<ArrayList<Integer>> dividing_nums_for_summing(ArrayList<ServerNode> servernodelist, ArrayList<Integer> nums)
 		{
 		ArrayList<ArrayList<Integer>> num_to_be_summed=new ArrayList<ArrayList<Integer>>();
-			for(int i=0;i<nodelist.size();i++)
-			{
+			for(int i=1;i<servernodelist.size();i++)
+		{
 				num_to_be_summed.add(new ArrayList<Integer>());
 			}
-			for(int i=0;i<nums.size();i++)
+			for(int i=1;i<=nums.size();i++)
 			{
-				int count=i%nodelist.size();
-				num_to_be_summed.get(count).add(nums.get(i));
+				int count=(i-1)%servernodelist.size();
+				num_to_be_summed.get(count).add(nums.get(i-1));
 			}
 			return num_to_be_summed;
 		}
@@ -44,75 +57,84 @@ public static ArrayList<Integer> sums_from_addingnodes=new ArrayList<Integer>();
 			public ClientNode(NodeIdentifier id, Network network) {
 				super(id, 5000, network);
 			}
-			public void sendSumMessage(ArrayList<Integer> nums, ArrayList<AddingNode> RegularServers, EntryServerNode other)  //only use on entryservernodes
+			
+			public void sendSumMessage(ArrayList<Integer> nums, ArrayList<ServerNode> servernodelist, ArrayList<ClientNode> clientnodelist,EntryServerNode other)  //only use on entryservernodes
 			{
-				other.sendAllMessages(this, RegularServers, nums);
+				int numsum=0;
+				this.sendMessage(other.getID(),new AddNumberMessage(this,numsum,nums,servernodelist,clientnodelist));
 			}
 			public void handleTimer() {
 				System.out.println(this.getID() + " timer event");
 			}
 			public void handleMessage(Message msg) {
 				System.out.println(this.getID() + " gets "+msg);
+				System.exit(0);
 			}
 		}
-		public static class EntryServerNode extends AddingNode {
-			public static int sumtotal=0;
-			public static ArrayList<Node> nodelist=new ArrayList<Node>();
+		public static class EntryServerNode extends ServerNode {
+			private int sumtotal=0;
+			private int count=0;
+			private int count2=0;
 			public EntryServerNode(NodeIdentifier id, Network network) {
 				super(id, network);
 			}
-			
 			@Override
 			public void handleTimer() {
 				System.out.println(this.getID() + " timer event");
 			}
 			
-			public void sendAllMessages(ClientNode client, ArrayList<AddingNode> nodelist, ArrayList<Integer> nums)
+			public void sendAllMessages(ClientNode client, ArrayList<ServerNode> servernodelist, ArrayList<ClientNode> clientnodelist, ArrayList<Integer> nums)
 			{
-				int k=nodelist.size();
-				ArrayList<ArrayList<Integer>> nums_to_be_summed=new ArrayList<ArrayList<Integer>>(dividing_nums_for_summing(nodelist,nums));
+				int k=servernodelist.size();
+				ArrayList<ArrayList<Integer>> nums_to_be_summed=new ArrayList<ArrayList<Integer>>(dividing_nums_for_summing(servernodelist,nums));
 				
 				for(int i=0;i<nums_to_be_summed.size();i++)
 				{
-					sendOneMessage(nodelist.get(i),nums_to_be_summed.get(i));
+					if(nums_to_be_summed.get(i).size()!=0)
+					{
+						count++;
+						sendOneMessage(servernodelist.get(i+1),nums_to_be_summed.get(i),servernodelist,clientnodelist);
+					}
 				}
-				for(int i: sums_from_addingnodes)
-				{
-					sumtotal+=i;
-				}
-				
-				try{ //delay so the earlier threads can execute
-					Thread.sleep(1000);
-				}
-				catch(InterruptedException e){
-					e.printStackTrace();
-				}
-				
-				this.sendBackMessage(client, sumtotal );
-			}
-			public void sendBackMessage(ClientNode other, int sum) //sending back to Client Node
-			{
-				this.sendMessage(other.getID(), new AddNumberMessage(this.getID(), sum));
-			}
-			public void sendOneMessage(AddingNode other, ArrayList<Integer> nums){ //sending to AddingNodes so that they can add numbers
-				other.sendOneMessage(this.getID(), nums);
-			}
-			public void handleMessage(Message msg) {
-				System.out.println(this.getID() + " gets "+msg);
-			}
-		}
-		public static class AddingNode extends Node{
-			public static int currentsum=0;
-			public AddingNode(NodeIdentifier id, Network network) {
-				super(id, 5000, network);
-			}
-
-			public void sendOneMessage(NodeIdentifier other, ArrayList<Integer> nums){
-				currentsum=addnums(nums);
-				sums_from_addingnodes.add(currentsum);
-				this.sendMessage(other, new AddNumberMessage(this.getID(), addnums(nums)));
 			}
 			
+			public void sendOneMessage(ServerNode other, ArrayList<Integer> nums,ArrayList<ServerNode> servernodelist,ArrayList<ClientNode> clientnodelist){ //sending to ServerNodes so that they can add numbers
+				int numsum=0;
+				this.sendMessage(other.getID(),new AddNumberMessage((Node)this,numsum,nums,servernodelist,clientnodelist));
+			}
+			public void handleMessage(Message msg) 
+			{
+				if(msg.getClass().equals(AddNumberMessage.class)) {
+				AddNumberMessage addmsg=(AddNumberMessage)msg;
+				if(addmsg.sender.getRole().equals(Role.CLIENT))
+				{
+					sendAllMessages((ClientNode)addmsg.senderNode,addmsg.servernodelist,addmsg.clientnodelist,addmsg.nums);
+				}
+				else if(addmsg.sender.getRole().equals(Role.SERVER))//then we are dealing with a servernode
+				{
+					count2++;
+					sumtotal+=addmsg.numsum; //this is how we add up the numbers
+					if(count2==count)
+						this.sendMessage(addmsg.clientnodelist.get(0).getID(), new AddNumberMessage(this,sumtotal)); //woops. sent message to the same class. derp. 
+				}
+			}
+			}
+		}
+		public static class ServerNode extends Node{
+			public ServerNode(NodeIdentifier id, Network network) {
+				super(id, 5000, network);
+			}
+			@Override
+			public void handleTimer() {
+				System.out.println(this.getID() + " timer event");
+			}
+			public void handleMessage(Message msg) 
+			{
+				if(msg.getClass().equals(AddNumberMessage.class)) {
+					AddNumberMessage addmsg=(AddNumberMessage)msg;
+					this.sendMessage(addmsg.sender, new AddNumberMessage(this, addnums(addmsg.nums), addmsg.nums,addmsg.servernodelist, addmsg.clientnodelist));
+				}
+			}
 			public int addnums(ArrayList<Integer>nums)
 			{
 				int count=0;
@@ -122,16 +144,6 @@ public static ArrayList<Integer> sums_from_addingnodes=new ArrayList<Integer>();
 				}
 				return count;
 			}
-			@Override
-			public void handleTimer() {
-				System.out.println(this.getID() + " timer event");
-			}
-
-			@Override
-			public void handleMessage(Message msg) {
-				currentsum=0;
-			}
-			
 		}
 		
 		public static void main(String args[]) throws Exception{
@@ -154,21 +166,23 @@ public static ArrayList<Integer> sums_from_addingnodes=new ArrayList<Integer>();
 			NodeIdentifier id2= new NodeIdentifier(Role.ENTRYSERVER, 2);
 			EntryServerNode node2 = new EntryServerNode(id2, network);
 			NodeIdentifier id3= new NodeIdentifier(Role.SERVER, 3);
-			AddingNode node3 = new AddingNode(id3, network);
+			ServerNode node3 = new ServerNode(id3, network);
 			NodeIdentifier id4= new NodeIdentifier(Role.SERVER, 4);
-			AddingNode node4 = new AddingNode(id4, network);
+			ServerNode node4 = new ServerNode(id4, network);
 			NodeIdentifier id5= new NodeIdentifier(Role.SERVER, 5);
-			AddingNode node5 = new AddingNode(id5, network);
+			ServerNode node5 = new ServerNode(id5, network);
 			
-			ArrayList<AddingNode> nodelist=new ArrayList<AddingNode>();
-			nodelist.add(node3); nodelist.add(node4); nodelist.add(node5);
+			ArrayList<ServerNode> servernodelist=new ArrayList<ServerNode>();
+			ArrayList<ClientNode> clientnodelist=new ArrayList<ClientNode>();
+			clientnodelist.add(node1);
+			servernodelist.add(node2); servernodelist.add(node3); servernodelist.add(node4); servernodelist.add(node5);
 			//Start experiment
 			node1.start();
 			node2.start();
 			node3.start();
 			node4.start();
 			node5.start();
-			node1.sendSumMessage(nums, nodelist, node2);
+			node1.sendSumMessage(nums, servernodelist, clientnodelist, node2);
 			
 		}
 }
